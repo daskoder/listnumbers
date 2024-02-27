d1d05ee9da88d95238a3d755f844fc924fc461f0
Marcus Pirita <piritamarcus@gmail.com>
2024-02-27 14:16:02 +0200

    Add project files.

---
+++ listnumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace listnumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("masiiv e array");
+
+            int[] numbers = { 2, 4, 6, 8 ,10 };
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("-------------------------------");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                
+                Console.WriteLine(numbers[i]= numbers[i] + 10);
+                
+
+                
+            }
+        }
+    }
+}
